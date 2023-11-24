aeedf99f33fb4c95a49cb3eed341037c29874912
Sankeerthana <sankeerthanasuddala@Sankeerthanas-MacBook-Pro.local>
2023-11-24 16:06:24 +0530

    Update with express app

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,12 @@
-FROM node 
-COPY . /app
-WORKDIR /app
-CMD node /app/app.js
+ARG NODE_VERSION=21.1.0
+
+FROM node:${NODE_VERSION}-alpine
+RUN mkdir -p /usr/app
+WORKDIR /usr/app
+COPY . . /usr/app/
+
+RUN npm install
+
+EXPOSE 3037
+
+CMD ["npm", "run", "start"]
--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hello Docker!");
+const express = require("express"),
+  app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.listen(3037, () => console.log(`App running on the port 3037`));
+app.get("/hello-world", (req, res) => res.send("Hello world !"));
